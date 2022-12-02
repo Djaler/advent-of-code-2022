451e019525a888f4bc40feb4327de07598ed07b8
Kirill Romanov <djaler1@gmail.com>
2022-12-02 20:58:03 +0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,33 @@
 fun main() {
+    fun <T> List<T>.split(delimeter: T): List<List<T>> {
+        val result = mutableListOf<List<T>>()
+        var buffer = mutableListOf<T>()
+        for (item in this) {
+            if (item != delimeter) {
+                buffer.add(item)
+            } else if (buffer.isNotEmpty()) {
+                result.add(buffer)
+                buffer = mutableListOf()
+            }
+        }
+        return result
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.split("")
+            .maxOf { elfSnacks -> elfSnacks.sumOf { it.toInt() } }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.split("")
+            .map { elfSnacks -> elfSnacks.sumOf { it.toInt() } }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
